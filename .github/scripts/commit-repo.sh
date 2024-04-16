47c2b60e55e79cfd9cb8c288a9423ce1cd607de2
ahmedgamal17 <12355.ahmedgamal.com@gmail.com>
2024-04-16 18:03:30 +0200

    some updates

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,8 +2,8 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore --exclude README.md --exclude repo.json ../main/repo/ .
-git config --global user.email "156378334+keiyoushi-bot@users.noreply.github.com"
-git config --global user.name "keiyoushi-bot"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
 git status
 if [ -n "$(git status --porcelain)" ]; then
     git add .
