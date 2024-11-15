c5d35d394a8e3460bdd14adce0950379fc395f35
ahmedgamal17 <12355.ahmedgamal.com@gmail.com>
2024-11-15 08:58:53 +0200

    S4U: remove cdn override

--- src/ar/scans4u/src/eu/kanade/tachiyomi/extension/ar/scans4u/Scans4u.kt
+++ src/ar/scans4u/src/eu/kanade/tachiyomi/extension/ar/scans4u/Scans4u.kt
@@ -10,6 +10,4 @@ class Scans4u : Keyoapp("Scans 4u", "https://4uscans.com", "ar") {
         }
         return "#chapters > a:not(:has(.text-sm span:matches(قادم)))"
     }
-
-    override val cdnUrl = "https://imageserver.is1.buzz/uploads"
 }
